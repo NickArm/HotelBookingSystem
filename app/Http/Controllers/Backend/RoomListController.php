cf2e4bde8314ff3d3cf679abbaace2cc88fb95ac
Nick_Arm <armenisnick@gmail.com>
2024-05-11 13:18:57 +0300

    Added New Booking Feature @AdminDashboard

--- app/Http/Controllers/Backend/RoomListController.php
+++ app/Http/Controllers/Backend/RoomListController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use App\Models\Room;
+use App\Models\RoomBookDate;
 use App\Models\RoomNumber;
+use App\Models\RoomType;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RoomListController extends Controller
 {
     public function ViewRoomList()
     {
-
-        // $room_number_list = RoomNumber
         $room_number_list = RoomNumber::with(['room_type', 'last_booking.booking:id,check_in,check_out,status,code,name,phone'])->orderBy('room_type_id', 'asc')
             ->leftJoin('room_types', 'room_types.id', 'room_numbers.room_type_id')
             ->leftJoin('booking_room_lists', 'booking_room_lists.room_number_id', 'room_numbers.id')
@@ -32,6 +38,107 @@ class RoomListController extends Controller
             ->get();
 
         return view('admin.backend.allroom.roomlist.view_roomlist', compact('room_number_list'));
+
+    }
+
+    public function AddRoomList()
+    {
+
+        $roomtype = RoomType::all();
+
+        return view('backend.allroom.roomlist.add_roomlist', compact('roomtype'));
+
+    }
+
+    public function StoreRoomList(Request $request)
+    {
+
+        if ($request->check_in == $request->check_out) {
+            $request->flash();
+            $notification = [
+                'message' => 'You Enter Same Date',
+                'alert-type' => 'error',
+            ];
+
+            return redirect()->back()->with($notification);
+        }
+
+        if ($request->available_room < $request->number_of_rooms) {
+            $request->flash();
+            $notification = [
+                'message' => 'You Enter Maximum Number of Rooms!',
+                'alert-type' => 'error',
+            ];
+
+            return redirect()->back()->with($notification);
+        }
+
+        $room = Room::find($request['room_id']);
+        if ($room->room_capacity < $request->number_of_person) {
+            $notification = [
+                'message' => 'You Enter Maximum Number of Guest!',
+                'alert-type' => 'error',
+            ];
+
+            return redirect()->back()->with($notification);
+        }
+
+        $toDate = Carbon::parse($request['check_in']);
+        $fromDate = Carbon::parse($request['check_out']);
+        $total_nights = $toDate->diffInDays($fromDate);
+
+        $subtotal = $room->price * $total_nights * $request->number_of_rooms;
+        $discount = ($room->discount / 100) * $subtotal;
+        $total_price = $subtotal - $discount;
+        $code = rand(000000000, 999999999);
+
+        $data = new Booking();
+        $data->room_id = $room->id;
+        $data->user_id = Auth::user()->id;
+        $data->check_in = date('Y-m-d', strtotime($request['check_in']));
+        $data->check_out = date('Y-m-d', strtotime($request['check_out']));
+        $data->persion = $request->number_of_person;
+        $data->number_of_rooms = $request->number_of_rooms;
+        $data->total_nights = $total_nights;
+
+        $data->actual_price = $room->price;
+        $data->subtotal = $subtotal;
+        $data->discount = $discount;
+        $data->total_price = $total_price;
+        $data->payment_method = 'COD';
+        $data->payment_status = 0;
+
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->country = $request->country;
+        $data->state = $request->state;
+        $data->zip_code = $request->zip_code;
+        $data->address = $request->address;
+
+        $data->code = $code;
+        $data->status = 0;
+        $data->created_at = Carbon::now();
+        $data->save();
+
+        $sdate = date('Y-m-d', strtotime($request['check_in']));
+        $edate = date('Y-m-d', strtotime($request['check_out']));
+        $eldate = Carbon::create($edate)->subDay();
+        $d_period = CarbonPeriod::create($sdate, $eldate);
+        foreach ($d_period as $period) {
+            $booked_dates = new RoomBookDate();
+            $booked_dates->booking_id = $data->id;
+            $booked_dates->room_id = $room->id;
+            $booked_dates->book_date = date('Y-m-d', strtotime($period));
+            $booked_dates->save();
+        }
+
+        $notification = [
+            'message' => 'Booking Added Successfully',
+            'alert-type' => 'success',
+        ];
+
+        return redirect()->back()->with($notification);
 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -111,6 +111,8 @@ Route::middleware(['auth'])->group(function () {
     Route::controller(RoomListController::class)->group(function () {
 
         Route::get('/view/room/list', 'ViewRoomList')->name('view.room.list');
+        Route::get('/add/room/list', 'AddRoomList')->name('add.room.list');
+        Route::post('/store/roomlist', 'StoreRoomList')->name('store.roomlist');
 
     });
 });
